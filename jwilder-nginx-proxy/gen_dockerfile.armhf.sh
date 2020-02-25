418a8a5c1c3e3dd621eb71927ddd1ebb904ba816
satackey <21271711+satackey@users.noreply.github.com>
2020-02-25 23:15:29 +0900

    Add jwilder-nginx-proxy

--- jwilder-nginx-proxy/gen_dockerfile.armhf.sh
+++ jwilder-nginx-proxy/gen_dockerfile.armhf.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-FOREGO_TAG="v0.16.1"
+FOREGO_TAG="$(cat Dockerfile | sed -n 's/.*releases\/download\/\(v[0-9\.]*\)\/forego.*/\1/p')"
 
 FOREGO_DOCKERFLE="
 FROM golang:1 AS forego-build
 WORKDIR /src
-RUN set -ex \
-    && go get github.com/tools/godep \
-    && git clone --branch='${FOREGO_TAG}' https://github.com/jwilder/forego \
-    && cd forego \
+RUN set -ex \\
+    && go get github.com/tools/godep \\
+    && git clone --branch='${FOREGO_TAG}' https://github.com/jwilder/forego \\
+    && cd forego \\
     && make forego
+
 
 "
 
-echo $FOREGO_DOCKERFLE$(cat Dockerfile) > Dockerfile.armhf
+echo "$FOREGO_DOCKERFLE$(cat Dockerfile)" > Dockerfile.armhf
 
-sed -ie 's/ADD https:\/\/github.com\/jwilder\/forego\/releases\/download\/v0\.16\.1/forego/COPY --from=forego-build \/src\/forego\/forego/g' Dockerfile.armhf
-sed -ie 's/docker-gen-linux-amd64/docker-gen-linux-armhf/g' Dockerfile.armhf
+gsed -i -E 's/ADD https:\/\/github.com\/jwilder\/forego\/releases\/download\/v0\.16\.1\/forego/COPY --from=forego-build \/src\/forego\/forego/g' Dockerfile.armhf
+gsed -i -E 's/docker-gen-linux-amd64/docker-gen-linux-armhf/g' Dockerfile.armhf
 
 echo ok
